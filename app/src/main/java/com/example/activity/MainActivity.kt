cff3b0773d4565a62488549c8e2b2f4da2e1eb12
JermieTapang <jermietapang96@gmail.com>
2025-05-14 21:57:00 +0400

    Initial commit

--- app/src/main/java/com/example/activity/MainActivity.kt
+++ app/src/main/java/com/example/activity/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-             }
+
+        // Find the button and set click listener
+        val buttonToSecond = findViewById<Button>(R.id.item_list)
+        buttonToSecond.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
 }
